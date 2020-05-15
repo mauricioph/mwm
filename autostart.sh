8310ca97bd596592ced8d36464259ba8790b45a1
mauricioph <jesusmlmph@gmail.com>
2020-05-15 20:07:17 +0100

    Add files via upload

--- autostart.sh
+++ autostart.sh
@@ -5,4 +5,7 @@ compton -i $HOME/.config/compton.conf &
 /usr/local/bin/wallpaper.script &
 conky &
 /usr/local/bin/battery-level &
-notify-send "Started all programmes"
+nmcli c up 3e5e8aa3-b981-496b-9fd2-ec767675d1e8 &
+sleep 30 &
+/opt/appimages/Nextcloud-2.6.4-x86_64.AppImage &
+notify-send "Started all programmes" &
