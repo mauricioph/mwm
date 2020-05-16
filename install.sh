c5f60542ad29c90ae9157e815837c30082e44520
mauricioph <jesusmlmph@gmail.com>
2020-05-16 15:25:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,12 +7,12 @@ cp autostart.sh dwmstatus poweroff.py "${casa}"
 chmod +x "${casa}" -R
 
 echo "Installing dwm, to control backlight and keyboard lights, please make sure to enable the sudo to run the scripts without password for more straight forward workflow"
-sleep 10
+sleep 5
 sudo cp screen-backlight keyboard-backlight /usr/local/bin
 sudo cp dwm.desktop /usr/share/xsessions/
 sudo make
 sudo make clean install
-sleep 10
+sleep 5
 
 clear
 echo "Add these lines to your sudoers file, replace user to your user or group
