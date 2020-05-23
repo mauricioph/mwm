6fa89223964a11f2ae14ea85b0f59ceaf260cdb1
mauricioph <jesusmlmph@gmail.com>
2020-05-23 13:42:56 +0100

    Update config.h

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const int borderalpha	    = 0xcc;	/* border alpha for windows */
+static const unsigned int borderpx  = 10;        /* border pixel of windows */
+static const int borderalpha	    = 0xbb;	/* border alpha for windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const int onegap		    = 0;	/* 1: diff gappx if one client 0: no gap if one client  */
 static const unsigned int onegappx  = 100;	/* single window gap px */
@@ -11,15 +11,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#c37306"; /* Bar colour */
+static const char col_gray2[]       = "#990000"; /* Secondary Window Border colour */
+static const char col_gray3[]       = "#880000"; /* All text colour */
+static const char col_gray4[]       = "#FFFFFF"; /* c37306 Text colour of Selection */
+static const char col_cyan[]        = "#1d4664"; /* Main Background Colour Of selection */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel] = { col_gray3, col_gray1, col_gray2 },
 };
 
 /* tagging */
@@ -32,8 +32,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1,            0,           -1 },
-	{ "i3-nagbar", NULL,      NULL,       0,            1,           -1 },
+/*	{ "Firefox",  NULL,       NULL,       0,            0,           -1 }, */
 };
 
 /* layout(s) */
@@ -45,11 +44,11 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-        { "[@]",      spiral }, /* Fibonnacci layout as default */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
- 	{ "[\\]",      dwindle }, /* Dwindle came with Fibonacci */
+    { "[Fibonacci]",      spiral }, /* Fibonnacci layout as default */
+	{ "[Tile]",      tile },    /* first entry is default */
+	{ "[Floating]",      NULL },    /* no layout function means floating behavior */
+	{ "[Full]",      monocle },
+ 	{ "[Dwindle]",      dwindle }, /* Dwindle is a corner kind of Fibonacci */
 };
 
 /* key definitions */
@@ -67,12 +66,12 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "rofi", "-show", "run", "-width 30" , NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *audioedtcmd[] = {"ocenaudio", NULL };
+static const char *audioedtcmd[] = {"audacity", NULL };
 static const char *vlccmd[] = {"vlc", NULL };
 static const char *firefoxcmd[] = {"firefox", NULL };
 static const char *codecmd[] = {"code", NULL };
 static const char *poweroffcmd[] = {"python2", "/home/mauricio/.config/dwm/poweroff.py", NULL };
-static const char *lockcmd[] = {"bash","/home/mauricio/.config/i3/lock-fusy.sh", NULL };
+static const char *lockcmd[] = {"bash","/home/mauricio/.config/dwm/lock-fusy.sh", NULL };
 static const char *connectnetcmd[] = {"nmcli", "c", "up", "3e5e8aa3-b981-496b-9fd2-ec767675d1e8", NULL };
 
 #include <X11/XF86keysym.h>
@@ -83,13 +82,13 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} },
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },       /* rofi menu */
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },        /* st Terminal */
-   	{ MODKEY|ShiftMask,             XK_a,      spawn,          {.v = audioedtcmd }},    /* Audio Editor */
-   	{ MODKEY|ShiftMask,             XK_v,      spawn,          {.v = vlccmd }},         /* VLC Media Player */
+   	{ MODKEY,                       XK_a,      spawn,          {.v = audioedtcmd }},    /* Audio Editor */
+   	{ MODKEY,                       XK_v,      spawn,          {.v = vlccmd }},         /* VLC Media Player */
    	{ MODKEY,                       XK_g,      spawn,          {.v = codecmd }},        /* VSCode */
-   	{ MODKEY,                       XK_f,      spawn,          {.v = firefoxcmd }},     /* Firefox */
+   	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = firefoxcmd }},     /* Firefox */
    	{ MODKEY,                       XK_e,      spawn,          {.v = poweroffcmd }},    /* Power off */
 	{ MODKEY|ShiftMask,             XK_t,      spawn,          {.v = lockcmd }},        /* Lock screen */
-	{ MODKEY,			XK_n,	   spawn,	   {.v = connectnetcmd }}, /* Connect wifi to flat */
+	{ MODKEY,						XK_n,	   spawn,		   {.v = connectnetcmd }}, /* Connect wifi to flat */
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -113,16 +112,17 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-        { MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+    { MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
  	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
  	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
-        { 0,              XF86XK_AudioRaiseVolume, spawn,          SHCMD("pactl set-sink-volume 0 +5%") },
-        { 0,              XF86XK_AudioLowerVolume, spawn,          SHCMD("pactl set-sink-volume 0 -5%") },
-        { 0,              XF86XK_AudioMute,        spawn,          SHCMD("pactl set-sink-volume 0 0") },
-        { 0,              XF86XK_MonBrightnessUp,  spawn,          SHCMD("sudo screen-backlight up") },
-        { 0,            XF86XK_MonBrightnessDown,  spawn,          SHCMD("sudo screen-backlight down") },
-        { 0,              XF86XK_KbdBrightnessUp,  spawn,          SHCMD("sudo keyboard-backlight total") },
-        { 0,            XF86XK_KbdBrightnessDown,  spawn,          SHCMD("sudo keyboard-backlight off") },
+	{ MODKEY,		            	XK_c,	   spawn, 	       SHCMD("mpv --no-cache --no-osc --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)") },
+    { 0,              XF86XK_AudioRaiseVolume, spawn,          SHCMD("pactl set-sink-volume 0 +5%") },
+    { 0,              XF86XK_AudioLowerVolume, spawn,          SHCMD("pactl set-sink-volume 0 -5%") },
+    { 0,              XF86XK_AudioMute,        spawn,          SHCMD("pactl set-sink-volume 0 0") },
+    { 0,              XF86XK_MonBrightnessUp,  spawn,          SHCMD("sudo screen-backlight up") },
+    { 0,            XF86XK_MonBrightnessDown,  spawn,          SHCMD("sudo screen-backlight down") },
+    { 0,              XF86XK_KbdBrightnessUp,  spawn,          SHCMD("sudo keyboard-backlight total") },
+    { 0,            XF86XK_KbdBrightnessDown,  spawn,          SHCMD("sudo keyboard-backlight off") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -150,4 +150,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
