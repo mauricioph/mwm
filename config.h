6bfd5902a3870045b5bd55b6b8360ad1bdff699e
Mauricio <jesusmlmph@gmail.com>
2020-10-27 17:54:15 +0000

    Added height to top bar

--- config.h
+++ config.h
@@ -9,6 +9,7 @@ static const unsigned int onegappx  = 100;	/* single window gap px */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 25;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_orange[]       = "#c37306"; /* Bar colour */
