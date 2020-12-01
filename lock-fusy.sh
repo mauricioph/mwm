309ee33ad33831f4c45367b7d68a868c07a008b5
Mauricio <jesusmlmph@gmail.com>
2020-12-01 16:22:50 +0000

    Autostart and lock screen fix

---
+++ lock-fusy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# mwm folder
+mwmfolder="$HOME/.config/mwm"
+# Icon
+icon="${mwmfolder}/icon.png"
+# Take a screenshot
+locker=$(mktemp /tmp/.lock.XXXXX.png)
+locked=$(basename ${locker})
+scrot ${mwmfolder}/${locked}
+
+if [ -f ${mwmfolder}/screen_locked.png ]
+then rm -f ${mwmfolder}/screen_locked.png
+fi
+
+# Pixellate it 10x and write the enter the password phrase.
+convert -resize 10% ${mwmfolder}/${locked} ${mwmfolder}/screen_locked2.png
+convert -resize 1000% ${mwmfolder}/screen_locked2.png ${mwmfolder}/screen_locked3.png
+rm ${mwmfolder}/screen_locked2.png
+convert ${mwmfolder}/screen_locked3.png  "$icon" -gravity center -composite -font Helvetica -pointsize 32 -draw "gravity South fill black text 3,14 'Enter the password' fill blue  text 1,14 'Enter the password'" ${mwmfolder}/screen_locked.png
+
+rm ${mwmfolder}/screen_locked3.png
+rm ${mwmfolder}/${locked}
+rm ${locker}
+# Lock screen displaying this image.
+i3lock -i ${mwmfolder}/screen_locked.png
+
+# Turn the screen off after a delay.
+# sleep 300; pgrep i3lock && xset dpms force off
--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 house="${HOME}/.config/mwm"
 ${house}/scripts/ligamento &
-# compton -i $HOME/.config/compton.conf &
-# /usr/local/bin/wallpaper.script &
-# conky &
-# /usr/local/bin/battery-level &
-# play /usr/share/sounds/Ps1\ startup\ sound.flac &
-#nmcli c up 3e5e8aa3-b981-496b-9fd2-ec767675d1e8 &
 sleep 30 &
-# /opt/appimages/Nextcloud-3.0.1-x86_64.AppImage &
 notify-send "Started all programmes" &
-# xautolock -time 5 -locker "/home/mauricio/.config/mwm/lock-fusy.sh" -secure -detectsleep &
+xautolock -time 5 -locker "/home/mauricio/.config/mwm/lock-fusy.sh" -secure -detectsleep &
 setxkbmap gb &
